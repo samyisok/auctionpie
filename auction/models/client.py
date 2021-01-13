f70e2d0d91292cc2d49c1478b56c8c6e5d8cba9e
Sergey M <magaster@gmail.com>
2021-01-14 01:47:33 +0300

    add models

---
+++ auction/models/client.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Client(models.Model):
+    FACES = [
+        (1, u"Физ лицо"),
+        (2, u"Юр лицо"),
+        (3, u"ИП"),
+        (4, u"Бюджет"),
+    ]
+
+    email = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=128)
+    cdate = models.TimeField(auto_now=False, auto_now_add=True)
+    mdate = models.TimeField(auto_now=True, auto_now_add=False)
+    company_id = models.IntegerField(default=1)
+    activated = models.BooleanField(default=False)
+    face_id = models.models.IntegerField(
+        choices=FACES,
+        default=1,
+    )
