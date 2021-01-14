ac5af42bcbb3335aa4eced4e2ea890c97054b085
Sergey M <magaster@gmail.com>
2021-01-14 20:02:29 +0300

    add bid model

--- auction/models/__init__.py
+++ auction/models/__init__.py
@@ -1,3 +1,4 @@
 from .client import Client
 from .client_data import ClientData
 from .product import Product
+from .bid import Bid
---
+++ auction/models/bid.py
@@ -0,0 +1,21 @@
+from django.db import models
+from .client import Client
+from .product import Product
+
+
+class Bid(models.Model):
+    STATUSES = [
+        ("active", "Active"),
+        ("deleted", "Deleted"),
+    ]
+
+    client = models.ForeignKey(
+        Client, on_delete=models.CASCADE, blank=False, null=False
+    )
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, blank=False, null=False
+    )
+    status = models.CharField(choices=STATUSES, default="active", max_length=32)
+    price = models.DecimalField(max_digits=11, decimal_places=2)
+    cdate = models.TimeField(auto_now=False, auto_now_add=True)
+    mdate = models.TimeField(auto_now=True, auto_now_add=False)
