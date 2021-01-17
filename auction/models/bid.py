73c3bf9c3ac52fb40934833eb2b92cf4097e702b
Sergey M <magaster@gmail.com>
2021-01-17 17:47:49 +0300

    add __str__ to bid model

--- auction/models/bid.py
+++ auction/models/bid.py
@@ -23,3 +23,6 @@ class Bid(models.Model):
     price = models.DecimalField(max_digits=11, decimal_places=2)
     cdate = models.DateTimeField(auto_now=False, auto_now_add=True)
     mdate = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return f"{self.product.name}: {self.price}"
