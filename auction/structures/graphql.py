740a6379f4d63b40ff8108e1fa8e3d1ff433245c
Sergey M <magaster@gmail.com>
2021-01-18 11:44:46 +0300

    fix debug info

--- auction/structures/graphql.py
+++ auction/structures/graphql.py
@@ -59,8 +59,6 @@ class ProductInput(Structure):
     def check_date(cls, v, values, **kwargs):
         if v is None:
             return v
-        # print(v)
-        print(values["end_date"])
         if v <= values["end_date"]:
             raise ValueError("start_date should be greated than end_date")
         return v
