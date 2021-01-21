13a696831d8a18378f4cacd4e1f7026efd6eca7f
Sergey M <magaster@gmail.com>
2021-01-21 20:55:21 +0300

    fix unused import

--- auction/models/deal.py
+++ auction/models/deal.py
@@ -2,7 +2,7 @@ from django.db import models
 from auction.models import Client
 from auction.models import Product
 from auction.models.base import ModelAbstract
-from billing.models import Bill, BillType, BillStatus
+from billing.models import Bill, BillType
 
 
 class Deal(ModelAbstract):
