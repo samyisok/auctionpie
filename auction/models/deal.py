edd3e26fd9b522072eb135030cb7a5297c7fff9a
Sergey M <magaster@gmail.com>
2021-01-21 20:51:10 +0300

    add create bills to deals

--- auction/models/deal.py
+++ auction/models/deal.py
@@ -2,7 +2,7 @@ from django.db import models
 from auction.models import Client
 from auction.models import Product
 from auction.models.base import ModelAbstract
-from billing.models import Bill
+from billing.models import Bill, BillType, BillStatus
 
 
 class Deal(ModelAbstract):
@@ -27,6 +27,56 @@ class Deal(ModelAbstract):
 
     def __str__(self):
         return f"{self.product.name}: {self.amount}"
+
+    def get_commission(self):
+        """По бизнес логике, клиент оплачивает коммиссионные за сервис"""
+        return self.amount * 20 / 100
+
+    def get_proceeds(self):
+        """Выручка, это цена продажи за вычетом коммиссионных"""
+        return self.amount - self.get_commission()
+
+    def create_bills(self):
+        """
+        Проведение бизнес транзакций
+
+        первый счет - списание у покупателя полной стоймости
+        второй счет - начисление продавцу полной стоймости
+        третий счет - списание у продовца коммиссионых
+        """
+        # TODO Возможно тут есть место для стратегии.
+
+        # первый счет - списание у покупателя
+        bill_sell = Bill.objects.create(
+            client=self.buyer,
+            bill_type=BillType.SELL,
+            amount=self.amount,
+        )
+
+        DealBill.objects.create(deal=self, bill=bill_sell)
+
+        # второй счет - начисление продавцу
+        bill_proceeds = Bill.objects.create(
+            client=self.product.seller,
+            bill_type=BillType.PROCEEDS,
+            amount=self.amount,
+        )
+
+        DealBill.objects.create(deal=self, bill=bill_proceeds)
+
+        # третий счет - списание коммиссионых
+        bill_commission = Bill.objects.create(
+            client=self.product.seller,
+            bill_type=BillType.COMMISSION,
+            amount=self.get_commission(),
+        )
+
+        DealBill.objects.create(deal=self, bill=bill_commission)
+
+    def finalize(self):
+        """финализируем сделку"""
+        self.create_bills(self)
+        # TODO send email
 
 
 class DealBill(ModelAbstract):
