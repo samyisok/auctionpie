1e3c883fc75f2d16c6590a068aaa275a7664489b
Sergey M <magaster@gmail.com>
2021-01-23 11:35:27 +0300

    add tsets Sell Commission Proceeds

--- billing/tests/strategies/test_bill_strategy.py
+++ billing/tests/strategies/test_bill_strategy.py
@@ -3,7 +3,12 @@ from billing.models import (
     Bill,
 )
 from billing.meta import BillType, BillStatus
-from billing.strategies import BillStrategyPrepay
+from billing.strategies import (
+    BillStrategyPrepay,
+    BillStrategySell,
+    BillStrategyCommission,
+    BillStrategyProceeds,
+)
 from auction.models import Client
 from decimal import Decimal
 
@@ -75,5 +80,101 @@ class BillStrategyPrepayTestCase(TestCase, PrecreateMixin):
 
         self.assertEqual(
             self.strategy.transaction.amount, self.bill_prepay.amount
+        )
+        self.assertEqual(bill.status, BillStatus.ACTIVATED)
+
+
+class BillStrategySellTestCase(TestCase, PrecreateMixin):
+    """sell"""
+
+    def setUp(self):
+        self.precreate_data()
+        self.strategy = BillStrategySell(bill=self.bill_sell)
+
+    def test_matches(self):
+        """ strategy should match bill prepay"""
+        self.assertEqual(self.strategy.matches(self.bill_sell), True)
+        self.assertEqual(self.strategy.matches(self.bill_prepay), False)
+
+    def test_bill_activate(self):
+        """ bill should be activated """
+        bill = self.strategy.bill_activate()
+        self.assertEqual(bill.status, BillStatus.ACTIVATED)
+
+    def test_transaction_create(self):
+        """ should create transaction """
+        tnx = self.strategy.transaction_create()
+        self.assertEqual(tnx.amount, -1 * self.bill_sell.amount)
+
+    def test_activate(self):
+        """ should activate """
+        bill = self.strategy.activate()
+
+        self.assertEqual(
+            self.strategy.transaction.amount, -1 * self.bill_sell.amount
+        )
+        self.assertEqual(bill.status, BillStatus.ACTIVATED)
+
+
+class BillStrategyCommissionTestCase(TestCase, PrecreateMixin):
+    """ Commission """
+
+    def setUp(self):
+        self.precreate_data()
+        self.strategy = BillStrategyCommission(bill=self.bill_commission)
+
+    def test_matches(self):
+        """ strategy should match bill prepay"""
+        self.assertEqual(self.strategy.matches(self.bill_commission), True)
+        self.assertEqual(self.strategy.matches(self.bill_prepay), False)
+
+    def test_bill_activate(self):
+        """ bill should be activated """
+        bill = self.strategy.bill_activate()
+        self.assertEqual(bill.status, BillStatus.ACTIVATED)
+
+    def test_transaction_create(self):
+        """ should create transaction """
+        tnx = self.strategy.transaction_create()
+        self.assertEqual(tnx.amount, -1 * self.bill_commission.amount)
+
+    def test_activate(self):
+        """ should activate """
+        bill = self.strategy.activate()
+
+        self.assertEqual(
+            self.strategy.transaction.amount, -1 * self.bill_commission.amount
+        )
+        self.assertEqual(bill.status, BillStatus.ACTIVATED)
+
+
+class BillStrategyProceedsTestCase(TestCase, PrecreateMixin):
+    """proceeds"""
+
+    def setUp(self):
+        self.precreate_data()
+        self.strategy = BillStrategyProceeds(bill=self.bill_proceeds)
+
+    def test_matches(self):
+        """ strategy should match bill prepay"""
+        self.assertEqual(self.strategy.matches(self.bill_proceeds), True)
+        self.assertEqual(self.strategy.matches(self.bill_prepay), False)
+
+    def test_bill_activate(self):
+        """ bill should be activated """
+        bill = self.strategy.bill_activate()
+        self.assertEqual(bill.status, BillStatus.ACTIVATED)
+
+    def test_transaction_create(self):
+        """ should create transaction """
+        tnx = self.strategy.transaction_create()
+        self.assertEqual(tnx.amount, self.bill_proceeds.amount)
+
+    def test_activate(self):
+        """ should activate """
+        bill = self.strategy.activate()
+
+        self.assertEqual(
+            self.strategy.transaction.amount, self.bill_proceeds.amount
         )
         self.assertEqual(bill.status, BillStatus.ACTIVATED)
